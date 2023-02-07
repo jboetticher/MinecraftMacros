2927f77f356f49c248c3c261ba1838f2828868a4
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-02-07 13:45:36 -0600

    autocrafter

--- src/craft.rs
+++ src/craft.rs
@@ -8,29 +8,32 @@ pub struct Craft {}
 // This should only work on a 1080x1920 screen
 impl MinecraftMacro for Craft {
     fn macro_loop(&self) {
-        println!("Starting craft");
-        // 1. Move to the search section
-        MouseCursor::move_abs(100, 100);
-        thread::sleep(time::Duration::from_millis(100));
-        println!("Step 1 finished");
-
-        // 2. Input the text
-        KeySequence("Melon").send();
-        thread::sleep(time::Duration::from_millis(100));
-        println!("Step 2 finished");
-
-        // 3. Select the first thing that pops up
-        MouseCursor::move_abs(100, 50);
+        // 1. Move to the search section & click
+        MouseCursor::move_abs(430, 300);
         thread::sleep(time::Duration::from_millis(100));
         MouseButton::LeftButton.press();
         MouseButton::LeftButton.release();
         thread::sleep(time::Duration::from_millis(100));
-        println!("Step 3 finished");
 
-        // 4. Move to the crafting section
-        MouseCursor::move_abs(300, 50);
+        // 2. Select the first thing that pops up in crafting menu
+        MouseCursor::move_abs(415, 400);
         thread::sleep(time::Duration::from_millis(100));
-        println!("Step 4 finished");
+        KeybdKey::LShiftKey.press();
+        MouseButton::LeftButton.press();
+        MouseButton::LeftButton.release();
+        KeybdKey::LShiftKey.release();
+        thread::sleep(time::Duration::from_millis(100));
+
+        // 3. Move elsewhere to click & ensure that the Q Key is ok
+        MouseCursor::move_abs(1450, 300);
+        thread::sleep(time::Duration::from_millis(100));
+        MouseButton::LeftButton.press();
+        MouseButton::LeftButton.release();
+        thread::sleep(time::Duration::from_millis(100));
+
+        // 3. Move to the crafting section
+        MouseCursor::move_abs(1450, 400);
+        thread::sleep(time::Duration::from_millis(100));
 
         // 5. Press Q 64 times
         for _ in 0..64 {
@@ -38,8 +41,6 @@ impl MinecraftMacro for Craft {
             KeybdKey::QKey.release();
             thread::sleep(time::Duration::from_millis(20));
         }
-        println!("Step 5 finished");
-
     }
 
     fn name(&self) -> String {
@@ -47,6 +48,6 @@ impl MinecraftMacro for Craft {
     }
 
     fn desc(&self) -> String {
-        "Rapidly crafts a melon.".to_string()
+        "Rapidly crafts and drops.".to_string()
     }
 }
